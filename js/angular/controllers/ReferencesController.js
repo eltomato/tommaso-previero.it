79ee35543bd1f92d5e71efaa75638f04b93acd09
Tommaso Previero <t.previero@gmail.com>
2016-08-01 22:07:10 +0000

    Aggiunta referenza Matteo

--- js/angular/controllers/ReferencesController.js
+++ js/angular/controllers/ReferencesController.js
@@ -5,6 +5,14 @@ angular.module('tommaso-previero.controllers')
 	$scope.active = 0;
 
 	$scope.references = [{
+		quote: 'I worked with Tommaso from March 2013 to June 2016 at Arca Assicurazioni (an insurance company based in Verona), when he left to go to Australia. <br>'+
+				'Tommaso is a skilled, fast-learning innovative and curious guy. <br>'+
+				'His main strenghts are for sure smartness, curiosity and proactivity. <br>'+
+				'It was a pleasure to share a little path of my working and personal life with him. <br>'+
+				'I believe that he\'s a very valuable person to work with. <br>'+
+				'I hope to work with him again sooner or later.',
+		author: 'Matteo Facchin, Vice Head of Web Development Department at Arca Assicurazioni'
+	},{
 		quote: 'You are the most beautiful, respectful and sweet person I know.',
 		author: 'Mum'
 	},{
